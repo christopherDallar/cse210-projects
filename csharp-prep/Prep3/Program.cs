3ab8864ff7e57d13e012234f09c76ba48b8e1498
christopherDallar <christopherdallar1234@gmail.com>
2024-01-14 00:01:06 -0400

    Learning Activity - C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guessNumber = -1;
+
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guessNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guessNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+        }
+     
+        Console.WriteLine("You guessed it!");
     }
 }
