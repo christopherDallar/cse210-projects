435d471f76ae35d93773b9e40e5eb83cc550e35b
christopherDallar <christopherdallar1234@gmail.com>
2024-01-13 23:28:27 -0400

    Learning Activity - C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+
+        }
+        else if (gradePercentage >= 80)
+        {
+
+            letter = "B";
+
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Letter Grade: {letter}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulate you passed.");
+        } else {
+            Console.WriteLine("Try again the next time, you can do it!");
+        }
     }
 }
