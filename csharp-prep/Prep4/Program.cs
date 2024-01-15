29715f0b9172077c5bf5bf9acb8e3c6dcccbcff9
christopherDallar <christopherdallar1234@gmail.com>
2024-01-14 21:07:10 -0400

    Learning Activity - C# Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> entryNumbers = new List<int>();
+        int entryNumber;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            entryNumber = int.Parse(Console.ReadLine());
+
+            if (entryNumber != 0) {
+                entryNumbers.Add(entryNumber);
+            }
+
+        } while (entryNumber != 0);
+
+        int sumNumbers = entryNumbers.Sum();
+        double avgNumbers = entryNumbers.Average();
+        int largestNumber = entryNumbers.Max();
+
+        Console.WriteLine($"The sum is: {sumNumbers}");
+        Console.WriteLine($"The average is: {avgNumbers}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+
     }
 }
