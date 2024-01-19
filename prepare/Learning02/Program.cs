8ad557a6cbe4202843340f7512fdb242ec8a640d
christopherDallar <christopherdallar1234@gmail.com>
2024-01-19 11:08:24 -0400

    Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,29 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._company = "Binaural";
+        job1._jobTitle = "Developer";
+        job1._startYear = 1;
+        job1._endYear = 2;
+
+        Job job2 = new Job();
+
+        job2._company = "Macdonald";
+        job2._jobTitle = "Cashier";
+        job2._startYear = 3;
+        job2._endYear = 4;
+
+        Resume resume = new Resume();
+
+        resume._name = "Christopher Gómez Dallar";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
+
+
     }
 }
