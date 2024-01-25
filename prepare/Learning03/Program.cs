b488387748c3f2581a350e39936a448f445e39fb
christopherDallar <christopherdallar1234@gmail.com>
2024-01-25 09:27:06 -0400

    Learning 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction[] fractions = {
+            new Fraction(),
+            new Fraction(5),
+            new Fraction(3,4),
+            new Fraction(1,3),
+        };
+
+        for (int i = 0; i < fractions.Length; i++)
+        {
+            Fraction item = fractions[i];
+
+            Console.WriteLine($"Fraction {i+1}");
+            Console.WriteLine($"top: {item.GetTop()}");
+            Console.WriteLine($"bottom: {item.GetBottom()}");
+            Console.WriteLine(item.GetFractionString());
+            Console.WriteLine(item.GetDecimalValue());
+            Console.WriteLine("");
+        }
+
     }
 }
