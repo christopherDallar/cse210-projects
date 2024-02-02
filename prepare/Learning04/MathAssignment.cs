2e9bb5750925d3e664451af77c61f848b0c21771
christopherDallar <christopherdallar1234@gmail.com>
2024-02-02 01:25:06 -0400

    Inheritance Learning Activity

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,19 @@
+
+using System;
+
+class MathAssignment : Assignment
+{
+  string _textBookSection;
+  string _problems;
+
+  public MathAssignment(string studentName, string topic, string textBookSection, string problems) : base(studentName, topic)
+  {
+    _textBookSection = textBookSection;
+    _problems = problems;
+  }
+
+  public string GetHomeWorkList()
+  {
+    return $"Section {_textBookSection} Problems {_problems}";
+  }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Christopher", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Maria", "Multiplication", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeWorkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Jose", "Multiplication", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
